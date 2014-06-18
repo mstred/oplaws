774085d9a3a67f0378d3a9e9c5217e75890e21a4
Edson Samuel Jr <samuedson@gmail.com>
2014-06-17 23:53:10 -0300

    creating op. laws methods

--- src/br/cin/ufpe/util/OperationalLaws.java
+++ src/br/cin/ufpe/util/OperationalLaws.java
@@ -1,14 +1,39 @@
 package br.cin.ufpe.util;
 
 /**
- * This class contains static methods for a 
- * formulae-based evaluation on the laws for 
- * Operational Analysis, in the context
- * of Systems' Performance Evaluation
+ * This class contains static methods for a formulae-based 
+ * evaluation on the laws for Operational Analysis, in the 
+ * context of Systems' Performance Evaluation
  * 
  * @see http://amzn.com/0471503363
  */
 public class OperationalLaws {
-    
-    
+
+    public static void utilization1(Number resourceThroughput, Number resourceServiceTime) {
+
+    }
+
+    public static void utilization2(Number systemThroughput, Number resourceServiceDemand) {
+        
+    }
+
+    public static void forcedFlow(Number systemThroughput, Number resourceVisits) {
+        
+    }
+
+    public static void little(Number resourceThroughput, Number resourceResponseTime) {
+        
+    }
+
+    public static void generalResponseTime(int times, Number resourceResponseTime, Number resourceVisits) {
+        
+    }
+
+    public static void interactiveResponseTime(Number jobs, Number systemThroughput, Number thinkTime) {
+        
+    }
+
+    public static void asymptoticBounds() {
+        
+    }
 }
