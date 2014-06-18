363406e8746a0fcdea0f680696c967249e64fd84
Edson Samuel Jr <samuedson@gmail.com>
2014-06-17 22:56:05 -0300

    Moving GUI constructor call to Main class; Changing names

--- src/br/cin/ufpe/Main.java
+++ src/br/cin/ufpe/Main.java
@@ -1,9 +1,27 @@
 package br.cin.ufpe;
 
+import br.cin.ufpe.gui.GUI;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
 
-
     public static void main(String[] args) {
-        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
     }
 }
--- src/br/cin/ufpe/gui/GUI.java
+++ src/br/cin/ufpe/gui/GUI.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.cin.ufpe.gui;
 
 /**
  *
  * @author Charles Bezerra
  */
-public class ADS extends javax.swing.JFrame {
+public class GUI extends javax.swing.JFrame {
     float tf1;
     float tf2;
     float tf3;
@@ -19,7 +13,7 @@ public class ADS extends javax.swing.JFrame {
     /**
      * Creates new form ADS
      */
-    public ADS() {
+    public GUI() {
         initComponents();
     }
 
@@ -282,41 +276,6 @@ public class ADS extends javax.swing.JFrame {
         tf5 = (tf1*tf2*tf3*tf4);
         jTextField5.setText(String.valueOf(tf5));
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ADS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ADS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ADS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ADS.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ADS().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
